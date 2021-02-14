f006ae0e6cedd60a24d94d1d5a98395d6c6a5c24
sujanbudathoki <sujanbudathoki24@gmail.com>
2021-02-14 16:50:47 +0545

    Add project files.

---
+++ HotelListing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace HotelListing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(
+                 path: "c://Logs//log-.txt",
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH-mm-ss.fff zzz} [{Level:u3}]{Message:lj}{Newline}{Exception}",
+                rollingInterval: RollingInterval.Day,
+                restrictedToMinimumLevel: LogEventLevel.Information
+                ).CreateLogger();
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .UseStartup<Startup>();
+    }
+}
